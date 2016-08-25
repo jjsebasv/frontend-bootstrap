40bea6fd9c4de3ace4b3b424f544d527a724fec0
Noelia Lopez <noelia.lopez@wolox.com.ar>
2016-08-25 16:46:00 -0300

    Uncss

---
+++ gulp/tasks/uncss.js
@@ -0,0 +1,16 @@
+import gulp from 'gulp';
+import uncss from 'gulp-uncss';
+
+const localConfig = {
+  src: ['./build/css/application.css', './build/**/*.js'],
+  dest: './build/css/out',
+  html: ['./build/**/*.html']
+};
+
+gulp.task('uncss', () => {
+  return gulp.src(localConfig.src)
+    .pipe(uncss({
+      html: localConfig.html
+    }))
+    .pipe(gulp.dest(localConfig.dest));
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -11,18 +11,20 @@ const localConfig = {
 };
 
 gulp.task('watch:scss', () => {
-  gulp.watch(localConfig.scssWatchedFiles, ['sass']);
+  gulp.watch(localConfig.scssWatchedFiles, () => {
+    runSequence('sass', 'uncss');
+  });
 });
 
 gulp.task('watch:js', () => {
   gulp.watch(localConfig.jsWatchedFiles, () => {
-    runSequence('scripts', 'inject');
+    runSequence('scripts', 'uncss', 'inject');
   });
 });
 
 gulp.task('watch:jade', () => {
   gulp.watch(localConfig.jadeWatchedFiles, () => {
-    runSequence('jade', 'inject');
+    runSequence('jade', 'uncss', 'inject');
   });
 });
 
